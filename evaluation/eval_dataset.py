45cfd34a0f6effb709ed33b13146453122d798ad
ABhardwaj123 <agastya.bhardwajdps@gmail.com>
2026-08-01 19:03:42 +0530

    set of questions with expected answer

---
+++ evaluation/eval_dataset.py
@@ -0,0 +1,7 @@
+test_set = [
+    {"question": "How old is Agastya?", "expected_phrase": "19-year-old"},
+    {"question": "What does Agastya enjoy doing in his free time", "expected_phrase": "badminton"},
+    {"question": "What does Agastya want to do for his career?", "expected_phrase": "machine learning engineer"},
+    {"question": "What does Agastya miss while staying in college", "expected_phrase": "misses the food"},
+    {"question": "Is Agastya planning to apply for internships?", "expected_phrase": "internships"},
+]
